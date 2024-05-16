21c30d0394e60c2125d641da2af7299369b43007
IvanM1995 <142050290+IvanM1995@users.noreply.github.com>
2024-05-16 20:13:10 -0300

    Update Alumno.java

--- src/Entidades/Alumno.java
+++ src/Entidades/Alumno.java
@@ -5,10 +5,77 @@
  */
 package Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Ivan
  */
 public class Alumno {
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaN;
+    private boolean activo;
+
+    public Alumno() {
+    }
+
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaN, boolean activo) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaN = fechaN;
+        this.activo = activo;
+    }
+
+    public Alumno(String apellido, String nombre, LocalDate fechaN, boolean activo) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaN = fechaN;
+        this.activo = activo;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaN() {
+        return fechaN;
+    }
+
+    public void setFechaN(LocalDate fechaN) {
+        this.fechaN = fechaN;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+            
     
 }
