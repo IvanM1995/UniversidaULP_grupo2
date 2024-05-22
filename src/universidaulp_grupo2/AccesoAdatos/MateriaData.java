3923bcbbb68eceb0befbbddd718ebece97917ce4
javier Martin <42529660+guidojaviermartin@users.noreply.github.com>
2024-05-22 02:30:24 -0300

    Completando metodos en materiaData
    Agregue modificar alumno y eliminar alumno.

--- src/universidaulp_grupo2/AccesoAdatos/MateriaData.java
+++ src/universidaulp_grupo2/AccesoAdatos/MateriaData.java
@@ -37,11 +37,9 @@ public class MateriaData {
             if(rs.next()) {
                 materia.setIdMateria(rs.getInt(1));
                 System.out.println("La materia: "+materia.getNombre()+" fue añadida con exito");
-                JOptionPane.showMessageDialog(null, "Materia ID: "+materia.getIdMateria()); 
-                
+                JOptionPane.showMessageDialog(null, "Materia ID: "+materia.getIdMateria());  
             }
-            ps.close();
-
+            ps.close(); 
         }catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia: "+ex.getMessage()); 
         }
@@ -72,10 +70,51 @@ public class MateriaData {
     return materia;
     }
     
-     public List<Materia> listarMateria(){
+     public void modificarMateria(Materia materia){
+        String sql = "UPDATE materia SET nombre = ?, año = ?, estado = ? WHERE idMateria = ?";
+        PreparedStatement ps = null;
+            try{
+                ps = con.prepareStatement(sql);
+                ps.setString(1, materia.getNombre());
+                ps.setInt(2, materia.getAnioMateria());
+                ps.setBoolean(3, materia.isActivo());
+                ps.setInt(4, materia.getIdMateria());
+                int fila = ps.executeUpdate();
+
+                if (fila == 1) {
+                JOptionPane.showMessageDialog(null, "Materia Modificada Exitosamente.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "La materia no existe o fue eliminada");
+                }
+            }catch(SQLException ex){
+                JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia "+ex.getMessage());
+            }
+    }
+    public void eliminarMateria(int id){
+        PreparedStatement ps = null;
+        try{
+            String sql = "UPDATE materia SET estado = ? WHERE idMateria = ? AND estado= ?";
+            ps=con.prepareStatement(sql);
+            ps.setBoolean(1, false);
+            ps.setInt(2, id);
+            ps.setBoolean(3, true);
+            int fila = ps.executeUpdate();
+            if(fila==1){
+                JOptionPane.showMessageDialog(null, "Materia eliminada con exito!");
+                System.out.println("La materia con Id: "+id+" fue elminada");
+            }else{
+                System.out.println("La materia con Id: "+id+" no se encuentra o ya fue elminada");
+            }
+           ps.close(); 
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Estado de eliminacion de materia: Fallida "+e.getMessage());
+        } 
+    }
+     
+    public List<Materia> listarMateria(){
         List<Materia> materias = new ArrayList<>();
         try {
-            String sql = "SELECT * FROM materia WHERE estado = 1 ";
+            String sql = "SELECT * FROM materia WHERE activo = 1 ";
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
              while (rs.next()) {
@@ -83,7 +122,7 @@ public class MateriaData {
                 materia.setIdMateria(rs.getInt("idMateria"));
                 materia.setNombre(rs.getString("nombre"));
                 materia.setAnioMateria(rs.getInt("anio"));
-                materia.setActivo(rs.getBoolean("estado"));
+                materia.setActivo(rs.getBoolean("activo"));
                 materias.add(materia);
              }
         ps.close();
@@ -91,6 +130,6 @@ public class MateriaData {
             JOptionPane.showMessageDialog(null, " Error al acceder a la tabla materia "+ex.getMessage());
         }
         return materias;
-        }
+    }
   
 }//Fin class
